211d86d1048c2dfd4c629f6001dfac2de4b15d42
Bigtruck2 <unconfigured@null.spigotmc.org>
2021-09-12 19:15:48 -0400

    movement and turning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sebas.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import java.util.ArrayList;
+
+@TeleOp(name = "Sebas")
+public class Sebas extends LinearOpMode {
+    private DcMotor right;
+    private DcMotor left;
+    private DcMotor arm;
+    private TouchSensor touchSensor;
+    private final ArrayList<Object> hardwareList = new ArrayList<>();
+    @Override
+    public void runOpMode() {
+        right = motor("rightMotor");
+        left = motor("leftMotor");
+        arm = motor("armMotor");
+        touchSensor = (TouchSensor) hardware(TouchSensor.class,"touch");
+        right.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (touchSensor.isPressed()) {
+                    if (gamepad1.left_stick_y > .25 || gamepad1.left_stick_y < -0.25) {
+                        left.setPower(-(0.5 * gamepad1.left_stick_y));
+                        right.setPower(-(0.5 * gamepad1.left_stick_y));
+                    } else {
+                        left.setPower(-(.3 * gamepad1.left_stick_x));
+                        right.setPower((.3 * gamepad1.left_stick_x));
+                    }
+                    arm.setPower(-(.5 * gamepad1.right_stick_y));
+                    telemUpdate();
+                }
+            }
+        }
+    }
+    public DcMotor motor(String s){
+        hardwareList.add(hardwareMap.dcMotor.contains(s) ? hardwareMap.dcMotor.get(s) : null);
+         return hardwareMap.dcMotor.contains(s) ? hardwareMap.dcMotor.get(s) : null;
+    }
+    public Object hardware(Class c, String s){
+        hardwareList.add(hardwareMap.get(c,s));
+        return hardwareMap.get(c,s);
+    }
+    public void telemUpdate(){
+        telemetry.addData("Left Power", left.getPower());
+        telemetry.addData("Right Power", right.getPower());
+        telemetry.addData("Arm Power",arm.getPower());
+        telemetry.addData("Touch Sensor Pressed", touchSensor.isPressed());
+        telemetry.update();
+    }
+}
